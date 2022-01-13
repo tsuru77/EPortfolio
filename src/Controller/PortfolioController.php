a09ba76d2acea74b1844a81f8e4b98f2af0e7d16
tsuru77 <57574188+tsuru77@users.noreply.github.com>
2022-01-14 00:05:28 +0100

    EPortfolio
    Mon portfolio sur le  WEB

---
+++ src/Controller/PortfolioController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PortfolioController extends AbstractController
+{
+    /**
+     * @Route("/home", name="page1")
+     */
+    public function index(): Response
+    {
+        return $this->render('portfolio/index.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+    /**
+     * @Route("/contact", name="page2")
+     */
+    public function contact(): Response
+    {
+        return $this->render('portfolio/contact.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+    /**
+     * @Route("/cv", name="page3")
+     */
+    public function cv(): Response
+    {
+        return $this->render('portfolio/cv.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+    /**
+     * @Route("/portfolio", name="page4")
+     */
+    public function portfolio(): Response
+    {
+        return $this->render('portfolio/portfolio.html.twig', [
+            'controller_name' => 'PortfolioController',
+        ]);
+    }
+}
